229a5e6d76966c6cb558071047730e1c4da0ab7d
Peter Pang <mv@live.cn>
2019-01-16 14:00:48 +0800

    Init.

---
+++ Controllers/RatingsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Sample_Istio_Rating.Dtos;
+
+namespace Sample_Istio_Rating.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RatingsController : ControllerBase
+    {
+        [HttpGet("{id}")]
+        public RatingDto Get(string id)
+        {
+            Random random = new Random();
+            return new RatingDto { Star = random.Next(1, 5) };
+        }
+    }
+}
